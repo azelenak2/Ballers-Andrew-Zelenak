31291121149570ad1946292e61ca28d4ab5af1bc
azelenak2 <78370382+azelenak2@users.noreply.github.com>
2021-03-12 09:52:27 -0500

    Add files via upload

---
+++ script/Scenes/instructions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class instructions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void StartPressed()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
